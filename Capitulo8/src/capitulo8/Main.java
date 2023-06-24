4978e17abddbe763b8a73d97bdd83c73a089185c
Jovelina <131676788+Jovelina@users.noreply.github.com>
2023-06-23 21:08:09 -0400

    Add files via upload

---
+++ Capitulo8/src/capitulo8/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package capitulo8;
+
+/**
+ *
+ * @author ficdev
+ */
+public class Main {
+    
+    
+int resultado = 10/ 0;
+
+System.out.println(resultado); } 
+
+catch (ArithmeticException e) {
+
+System.out.println("Não é possível dividir por zero.");
+}
+    
+}
+
+    
+}
