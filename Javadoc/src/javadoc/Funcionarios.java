ee5d3ce0e9bd68400afc3f80c08feb73d7edec10
Jovelina <131676788+Jovelina@users.noreply.github.com>
2023-06-23 21:20:16 -0400

    Add files via upload

---
+++ Javadoc/src/javadoc/Funcionarios.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javadoc;
+
+/**Classe para objetoa do tipo Funcionários, onde serão contidos
+ *
+ * @author Manoel Pimentel
+ * @version 1.05
+ * @since Release 02 da aplicação
+ */
+
+public class Funcionarios {
+    
+    private String matricula;
+    private Double salario;
+    
+    public String getMatricula() {
+        return this.matricula;
+        
+    }
+    
+/** Método para retorno do salário do funcionário
+ * @return Double - Valor do Salário */
+    
+public Double getSalario() {
+    return this.salario;
+}
+
+/**Método para calculo da diária com base no salário do funcionário
+ * funcionário dividido pelo mês comercial de 30 dias para efetuar o pagamento
+ * @author Emanuel Silva
+ * @param diasViagem int - Valor total das vendas do mês.
+     * @param valorDiaria
+ * @return Double - Valor da diaria
+ */
+
+public Double calculaAjudaCusto(int diasViagem, Double valorDiaria);
+thystatic {
+}
+}catch (){
+}   
+
+/**Método para calculo do valor da bonificação baseada na 
+ * seguinte faixa de valores: Para vendas menores de
+ * 25.000,00 o percentual de comissão aplicado será de 5%, e
+ * será de 10%
+ * @author Manoel Pimentel
+ * @param valorVendas - Valor total das vendas do mês
+ * @return Double - Valor do resultado do cálculo conforme a f
+ */
+
+public Double calculaBonificacao(Double valorVendas){
+
+}
+if (valorVendas <25000.00){
+return this.salario * 0.05;
+
+} else 
+
+{
